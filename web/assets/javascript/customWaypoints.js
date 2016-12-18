92a102ecc7c9090dfebf3b1e391379e79517d7fa
Bogdan C <tunetu_bog@yahoo.com>
2016-12-18 04:30:26 +0200

    [PAW-5088542] Prima pagina

---
+++ web/assets/javascript/customWaypoints.js
@@ -0,0 +1,44 @@
+var InitWaypoint = function(elemName, elemNameEnable, offset, scrollTopCorrection) {
+    this.elemName = elemName;
+    this.elemNameEnable = elemNameEnable;
+    this.offset = offset;
+    this.scrollTopCorrection = scrollTopCorrection;
+    this.waypointEnabled = true;
+};
+
+InitWaypoint.prototype = function() {
+    var handleWaypoint = function(){
+            var thisObj = this;
+            var waypoint = new Waypoint({
+                element: document.getElementById(thisObj.elemName),
+                handler: function (direction) {
+                    if (thisObj.waypointEnabled) {
+                        $('html, body').animate({
+                            scrollTop: $("#" + thisObj.elemName).offset().top - thisObj.scrollTopCorrection
+                        }, 500);
+
+                        thisObj.waypointEnabled = false;
+                    }
+                },
+                offset: thisObj.offset
+            });
+        },
+        processEnableWaypoint = function() {
+            var thisObj = this;
+            var waypointAboutEnable = new Waypoint({
+                element: document.getElementById(thisObj.elemNameEnable),
+                handler: function(direction) {
+                    setTimeout(function() {
+                        thisObj.waypointEnabled = true;
+                    }, 25);
+                    thisObj.waypointEnabled = false;
+                },
+                offset: '30%'
+            });
+        };
+
+    return {
+        handleWaypoint: handleWaypoint,
+        processEnableWaypoint: processEnableWaypoint
+    };
+}();
