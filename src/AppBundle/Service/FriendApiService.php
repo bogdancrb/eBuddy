fefd908fcf72e5191e24b8bea6fa67a4fd924227
Bogdan C <tunetu_bog@yahoo.com>
2017-01-14 23:02:32 +0200

    [PAW-5069377] Create entities

---
+++ src/AppBundle/Service/FriendApiService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bogdan
+ * Date: 14.01.2017
+ * Time: 18:39
+ */
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Relationship;
+
+class FriendApiService extends BaseService
+{
+	const SERVICE_NAME = 'api.friend.service';
+	
+}
